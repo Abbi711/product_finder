ddc162627fab0e5d4d5fb242460fee77ab145c79
Abbinaiya-Vijayaraghavan1 <56724139+Abbinaiya-Vijayaraghavan1@users.noreply.github.com>
2020-10-13 13:43:46 +0000

    Create DBconnect.php

---
+++ DBconnect.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "product_finder";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+?>
